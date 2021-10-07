276146e8aac715fd1497bf062672600770f68501
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-10-07 21:16:59 +0200

    Update to homework
    Now Form validation works with the help of Yup and Formik packages. Even could find out from Documentation how to validate with Yup password and repeated password so they match each other.  Great success.

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,7 +2,7 @@ import {withRouter} from "react-router-dom";
 
 function MyFooter() {
   return (
-    <footer className="d-flex flex-column align-items-center bg-dark pb-3 mt-5 fixed-bottom">
+    <footer className="d-flex flex-column align-items-center bg-dark pb-3 mt-5">
       <h3 className="text-muted">Footer section</h3>
       <p className="m-0 p-0 text-white">Bookstore - 2021</p>
     </footer>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,11 +2,35 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import {useState, useEffect} from 'react';
+import Col from 'react-bootstrap/Col';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { FormControl } from 'react-bootstrap';
+import { useEffect } from 'react';
+
+const validationSchema = yup.object().shape({
+  firstName: yup.string().min(2, "Too short first name").required("First name is required"),
+  lastName: yup.string().min(3, "Too short last name").required("Last name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().min(8, "Too short password").required("Password is required"),
+  repeatedPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords do not match!").required("Type in your password again"),
+})
 
 function Register() {
 
-  const[firstName, setFirstName] = useState("");
+const {values, handleChange, handleSubmit, errors, touched, isValid, isValidating, status} = useFormik({
+  initialValues: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    repeatedPassword: '',
+  }, 
+  onSubmit: (values) => {alert(JSON.stringify(values))},
+  validationSchema: validationSchema
+})
+
+/*   const[firstName, setFirstName] = useState("");
   const[lastName, setLastName] = useState("");
   const[email, setEmail] = useState("");
   const[password, setPassword] = useState("");
@@ -19,116 +43,55 @@ function Register() {
     lastName: true,
     password: true,
     repeatedPassword: true,
-  })
+  }) */
 
-
-  useEffect(() => {
-
-    console.log(errors);
-
-    if(firstName.length < 2) {
-      setErrors({
-        ...errors,
-        firstname: true
-      })
-    } else {
-      setErrors({
-        ...errors,
-        firstname: false
-      })
-    }
-
-    if(lastName.length < 3) {
-      setErrors({
-        ...errors,
-        lastname: true
-      })
-    } else {
-      setErrors({
-        ...errors,
-        lastname: false
-      })
-    }
-
-    if(password.length < 8) {
-      setErrors({
-        ...errors,
-        password: true
-      })
-    } else {
-      setErrors({
-        ...errors,
-        password: false
-      })
-    }
-
-    if(password.length > 7 && password !== passwordRepeated) {
-      setErrors({
-        ...errors,
-        repeatedPassword: false
-      })
-    } else {
-      setErrors({
-        ...errors,
-        repeatedPassword: true
-      })
-    }
-
-      for(const error in errors) {
-      if(errors[error] === true) {
-        setSubmitButton(true);
-        return;
-      } else {
-        setSubmitButton(false);
-      }
-    }
-
-  }, [firstName, lastName, email, password, passwordRepeated])
   // Name (2 chars), Surname (3 chars), Email (email field), password(8 chars, 1 digit, 1 letter)
   // PasswordConfirm - same value as password
+
+
   return (
-  
   <Container className="mt-5">
     <Row className="justify-content-center">
-      <Form onSubmit={(event) => {event.preventDefault()}} className="mt-5">
+      <Col md={8}>
+      <Form onSubmit={handleSubmit} className="mt-5">
+
         <Form.Group controlId="formGroupFirstName">
           <Form.Label>First name</Form.Label>
-          <Form.Control required type="text" placeholder="Enter first name" value={firstName} onChange={(event) => {setFirstName(event.target.value)}} />
-        <Form.Text id="passwordHelpBlock" muted>
-          {errors.firstName && 'Your first name must contain at least 2 characters'}
-        </Form.Text>
+          <Form.Control isInvalid={errors.firstName} placeholder="Enter your first name" value={values.firstName} name="firstName" onChange={handleChange} />
+        <FormControl.Feedback type={errors.firstName ? "invalid" : "valid"}>{errors.firstName}</FormControl.Feedback>
         </Form.Group>
+
         <Form.Group controlId="formGroupLastName">
           <Form.Label>Last name</Form.Label>
-          <Form.Control type="text" placeholder="Enter last name" value={lastName} onChange={(event) => {setLastName(event.target.value)}}/>
-        <Form.Text id="passwordHelpBlock" muted>
-          {errors.lastName && 'Your last name must contain at least 3 characters'}
-        </Form.Text>
+          <Form.Control isInvalid={errors.lastName} placeholder="Enter last name" value={values.lastName} name="lastName" onChange={handleChange}/>
+        <FormControl.Feedback type={errors.lastName ? "invalid" : "valid"}>{errors.lastName}</FormControl.Feedback>
         </Form.Group>
+
         <Form.Group controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control required type="email" placeholder="Enter your email" value={email} onChange={(event) => {setEmail(event.target.value)}} />
+          <Form.Control isInvalid={errors.email} placeholder="Enter your email" value={values.email} name="email" onChange={handleChange}/>
+          <FormControl.Feedback type={errors.email ? "invalid" : "valid"}>{errors.email}</FormControl.Feedback>
         </Form.Group>
+
+
         <Form.Group controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control required type="password" placeholder="Password" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
-        <Form.Text id="passwordHelpBlock" muted> 
-        Your password must be at least 8 characters long, contain 1 letter and 1 numbers, and must not contain spaces, special characters, or emoji.
-        </Form.Text>
+          <Form.Control isInvalid={errors.password} placeholder="Password" value={values.password} name="password" onChange={handleChange}/>
+        <FormControl.Feedback type={errors.password ? "invalid" : "valid"}>{errors.password}</FormControl.Feedback>
         </Form.Group>
-        <Form.Group controlId="formGroupPasswordAgain">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Repeat password" value={passwordRepeated} onChange={(event) => {setPasswordRepeated(event.target.value)}} />
-          <Form.Text id="passwordHelpBlock" muted> 
-          {
-            errors.repeatedPassword && 'Passwords do not match each other'
-          }
-        </Form.Text>
+
+
+        <Form.Group controlId="formGroupRepeatPassword">
+          <Form.Label>Repeat Password</Form.Label>
+          <Form.Control isInvalid={errors.repeatedPassword} placeholder="Repeat password" value={values.repeatedPassword} name="repeatedPassword" onChange={handleChange}/>
+        <FormControl.Feedback type={errors.repeatedPassword ? "invalid" : "valid"}>{errors.repeatedPassword}</FormControl.Feedback>
         </Form.Group>
-        <Button variant="secondary" type="submit" disabled={submitButton}>
-        Submit
-      </Button>
+
+        <Button variant="secondary" type="submit" disabled={isValid ? false : true}>
+          Submit
+        </Button>
       </Form>
+      </Col>
     </Row>
   </Container>
   )
